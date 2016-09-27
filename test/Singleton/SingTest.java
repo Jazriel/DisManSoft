5c76d7a56a12dc5c9c45472f1acb0996dedbff5e
JavierMartinezRiberas <jmr0082@alu.ubu.es>
2016-09-27 20:25:28 +0200

    Minor changes to test.

--- test/Singleton/SingTest.java
+++ test/Singleton/SingTest.java
@@ -2,15 +2,23 @@ package Singleton;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 import Singleton.PrinterSingleton;
 
 public class SingTest {
 
+	private IPrinter printer;
+	
+	@Before
+	public void beforeTests(){
+		printer = PrinterSingleton.getInstance();
+	}
+	
+	
 	@Test
 	public void testGetInstance() {
-		IPrinter pr = PrinterSingleton.getInstance();
-		assertTrue(pr==PrinterSingleton.getInstance());
+		assertTrue(printer==PrinterSingleton.getInstance());
 	}
 	
 }
